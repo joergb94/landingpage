617b63261cc41e2c60d896dcb1c501921fc6a94a
Jose Gamboa <joseraymundogamboa30@gmail.com>
2021-01-05 13:03:06 -0600

    corrections headers

--- resources/views/website/items/header.blade.php
+++ resources/views/website/items/header.blade.php
@@ -1,22 +1,21 @@
 <header id="header" class="">
         <div class="header-content">
         <img style="width:100%; height:90vh;" src="{{ asset('img/Cover.png') }}" alt="Chania">
-        <div class="card-header head-dp col-sm-12 text-center" style="height:10vh;">
-                <template v-if="menu==0">
-                    <h1 style="display:none" class="text-center text-white title-head"> Welcome </h1>
-                </template>
-                <template v-if="menu==1">
-                        <h1 style="display:none" class="text-center text-white title-head"> About Us</h1>
-                </template>
-                <template v-if="menu==2">
-                        <h1 class="text-center text-white title-head"> Our Service</h1>
-                </template>
-                <template v-if="menu==3">
-                        <h1 style="display:none" class="text-center text-white title-head">Careers</h1>
-                </template>
-                <template v-if="menu==4">
-                        <h1 style="display:none" class="text-center text-white title-head">Contact Us</h1>
-                </template> 
-        </div>       <!-- end of container -->
+       
+                        <div v-if="menu==1" class="card-header head-dp col-sm-12 text-center" style="height:10vh;">
+                                <h1 style="display:none" class="text-center text-white title-head"> About Us</h1>
+                        </div>  
+                        <div v-if="menu==2" class="card-header head-dp col-sm-12 text-center" style="height:10vh;">
+                                <h1 class="text-center text-white title-head"> Our Service</h1>
+                        </div>  
+                
+                        <div v-if="menu==3" class="card-header head-dp col-sm-12 text-center" style="height:10vh;">
+                                <h1 style="display:none" class="text-center text-white title-head">Careers</h1>
+                        </div>         
+      
+                        <div v-if="menu==4" class="card-header head-dp col-sm-12 text-center" style="height:10vh;">
+                                <h1 style="display:none" class="text-center text-white title-head">Contact Us</h1>
+                        </div>
+          
         </div> <!-- end of header-content -->
     </header>
