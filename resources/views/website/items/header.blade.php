a78126a9de9246c890f075907398916ecb09fb36
Jose Gamboa <joseraymundogamboa30@gmail.com>
2021-01-08 18:56:51 -0600

    changes

--- resources/views/website/items/header.blade.php
+++ resources/views/website/items/header.blade.php
@@ -1,8 +1,9 @@
 
 <header id="header" class="">
-        <div  class="container-yellow" style="width:100%; height:90vh;" >
+        <div  class="container-yellow" style="width:100%; height:95vh;" >
                         <img class="img-head-dp" src="{{ asset('img/Cover.png') }}" alt="DedicatedPeople">
                         <div class="centered-head-dp">
+                            <img class="img-head img-fluid max-h" src="{{ asset('img/logoDP.png') }}" alt="DedicatedPeople">
                             <h1 id="head-i-y" class="text-head"></h1>
                             <br>
                         </div>
