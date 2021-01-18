fbd3ddfb0c560eca3244e16cbacb65cee12465f2
Jose Gamboa <joseraymundogamboa30@gmail.com>
2021-01-18 15:21:01 -0600

    update header

--- resources/views/website/items/header.blade.php
+++ resources/views/website/items/header.blade.php
@@ -3,7 +3,7 @@
         <div  class="container-yellow" style="width:100%; height:95vh;" >
                         <img class="img-head-dp" src="{{ asset('img/Cover.png') }}" alt="DedicatedPeople">
                         <div class="centered-head-dp">
-                            <h1 id="head-i-y" class="text-head">STAFFING MADE EASY</h1>
+                            <h1 id="head-i-y" class="text-head">STAFFING 24/7 TO MEET YOUR NEEDS</h1>
                             <br>
                         </div>
                     </div> 
