c63d903d9f6bab41347e6baafe383d69d96b9e44
Jorge Andres Puero Loeza <andres.puerto@dedicatedemail.us>
2021-04-13 13:20:52 -0500

    Change cover and linkedin
    Cover changed and add a linkedin url

--- resources/views/website/items/btn-social.blade.php
+++ resources/views/website/items/btn-social.blade.php
@@ -5,4 +5,7 @@
     <a class ="btn btn-link nav-link page-scroll" target="_blank" href="https://www.computrabajo.com.mx/empresas/acerca-de-dedicated-people-9A15B15DF634FE84" data-toggle="tooltip" title="Compu Trabajo DP!">
         <i style="font-size: 16px;" class="lni lni-world"><strong>CT</strong></i>
     </a>
+    <a class="btn btn-link nav-link page-scroll" target="_blank" href="https://www.linkedin.com/company/dedicated-people-llc/mycompany/?viewAsMember=true" data-toggle="tooltip" title="Linkedin DP!">
+    	<i style="font-size: 16px;" class="lni lni-linkedin-original"></i>
+    </a>
 </div>
