dd9e53fdeedaca0cd49b2fa3fb7f565b8571b280
joergb94 <67659588+joergb94@users.noreply.github.com>
2023-01-04 11:31:31 -0800

    Bug#1 System: Correction for div map for all displays

--- resources/views/website/items/contact.blade.php
+++ resources/views/website/items/contact.blade.php
@@ -36,9 +36,9 @@
                  <br>
                     <iframe id="contact-frame" style="overflow-x: hidden;" src="https://docs.google.com/forms/d/e/1FAIpQLSftVIehd2uKeYe5eYNAUGo5jGU3Y8OiTYN6xUP0_WUVtx_XKQ/viewform?embedded=true" width="100%" height="1350" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
              </div>
-             <div class="col-sm-7">
+             <div class="col-sm-7 d-none d-sm-none d-md-none d-lg-block">
             
-                <iframe class="map-frame" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3724.7984209960796!2d-89.604403!3d21.000716!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f5676caf8556c7f%3A0xaf1b2c05730b21fd!2sCalle%2020%20212A%2C%20Col.%20M%C3%A9xico%2C%20M%C3%A9xico%20Oriente%2C%2097137%20M%C3%A9rida%2C%20Yuc.!5e0!3m2!1ses!2smx!4v1612898970272!5m2!1ses!2smx" width="100%" height="60%" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                <iframe class="map-frame " src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3724.7984209960796!2d-89.604403!3d21.000716!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f5676caf8556c7f%3A0xaf1b2c05730b21fd!2sCalle%2020%20212A%2C%20Col.%20M%C3%A9xico%2C%20M%C3%A9xico%20Oriente%2C%2097137%20M%C3%A9rida%2C%20Yuc.!5e0!3m2!1ses!2smx!4v1612898970272!5m2!1ses!2smx" width="100%" height="60%" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
              </div> 
          </div> <!-- end of col -->
     </div>
